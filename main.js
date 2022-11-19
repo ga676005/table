05759f676d33389037ca3123f7c3cb0940810546
gohomewho <ga676005@hotmail.com.tw>
2022-11-19 18:02:03 +0800

    add column data formatter

--- main.js
+++ main.js
@@ -237,15 +237,37 @@ const nameOfDefaultColumns = [
   'email',
   'phone',
   'website',
+  'company',
+  'address'
 ]
 
-const table = createTable(nameOfDefaultColumns, users)
+const columnFormatter = {
+  'company': (data) => {
+    const p = document.createElement('p')
+    const strong = document.createElement('strong')
+    strong.textContent = ` (${data.catchPhrase})`
+    p.append(data.name, strong)
+    return p
+  },
+  'address': (data) => {
+    const {
+      street,
+      suite,
+      city,
+      zipcode,
+    } = data
+
+    return `${street} ${suite} ${city} ${zipcode}`
+  }
+}
+
+const table = createTable(nameOfDefaultColumns, users, columnFormatter)
 document.body.appendChild(table)
 
-function createTable(columns, dataList) {
+function createTable(columns, dataList, columnFormatter) {
   const table = document.createElement('table')
   const thead = createTableHead(columns)
-  const tbody = createTableBody(columns, dataList)
+  const tbody = createTableBody(columns, dataList, columnFormatter)
   table.appendChild(thead)
   table.appendChild(tbody)
   return table
@@ -255,9 +277,9 @@ function createTableHead(columns) {
   const thead = document.createElement('thead')
   const tr = document.createElement('tr')
 
-  columns.forEach(name => {
+  columns.forEach(columnName => {
     const th = document.createElement('th')
-    th.textContent = name
+    th.textContent = columnName
     tr.appendChild(th)
   });
 
@@ -265,20 +287,36 @@ function createTableHead(columns) {
   return thead
 }
 
-function createTableBody(columns, dataList) {
+function createTableBody(columns, dataList, columnFormatter) {
   const tbody = document.createElement('tbody')
 
   dataList.forEach(eachDataObject => {
-    const tr = document.createElement('tr')
-
-    columns.forEach((columnName) => {
-      const td = document.createElement('td')
-      td.textContent = eachDataObject[columnName]
-      tr.appendChild(td)
-    })
-
+    const tr = createTableRow(columns, eachDataObject, columnFormatter)
     tbody.appendChild(tr)
   });
 
   return tbody
+}
+
+function createTableRow(columns, dataOfTheRow, columnFormatter) {
+  const tr = document.createElement('tr')
+  columns.forEach((columnName) => {
+    const td = document.createElement('td')
+    const columnValue = dataOfTheRow[columnName]
+
+    if (columnFormatter && columnFormatter[columnName]) {
+      const formatterOfColumn = columnFormatter[columnName]
+      if (formatterOfColumn) {
+        const formatted = formatterOfColumn(columnValue)
+        td.append(formatted)
+      }
+    }
+    else {
+      td.textContent = columnValue
+    }
+
+    tr.appendChild(td)
+  })
+
+  return tr
 }
